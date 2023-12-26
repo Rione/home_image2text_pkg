ba37556144dbe15d4a366ed87ee10e9f82e2cba0
H-goto16 <h.goto.engineer@gmail.com>
2023-12-26 21:33:53 +0900

    feat: create i2t_node

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
 import rospy
+from image2text import i2t
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
-from i2t import i2t_ros
+from std_msgs.msg import String
 
-def start_node():
-    rospy.init_node("image2text")
-    rospy.roginfo("image2text node started.")
-    rospy.Subscriber("image_raw", Image, i2t_ros)
+class i2t_node:
+    def __init__(self):
+        self.i2t = i2t()
+        self.sub = rospy.Subscriber("image_raw", Image, self.callback)
+        self.pub = rospy.Publisher("i2t_text", String, queue_size=1)
+
+    def callback(self, msg):
+        bridge = CvBridge()
+        image_raw = bridge.imgmsg_to_cv2(msg, "bgr8")
+        text = self.i2t.image2text(image_raw)
+        self.pub.publish(text)
+
+if __name__ == "__main__":
+    rospy.init_node("i2t_node")
+    i2t_node()
     rospy.spin()
+
